6524f6c8cbbc7a83738491793c02a65b02fecae9
rt3310 <btac3310@gmail.com>
2022-01-22 21:07:45 +0900

    update til

--- Algorithm/programmers/sort/maximum.py
+++ Algorithm/programmers/sort/maximum.py
@@ -1,14 +1,27 @@
 def solution(numbers):
     answer = ''
     
+    string = list(map(str, numbers))
     mul = []
-    for v in numbers:
+    result = []
+    for i, v in enumerate(string):
         tmp = []
-        for i in range(len(str(v)) - 1, -1, -1):
-            tmp.append((v // (10**i)) % 10)
+        tmp.append(v + (v[0] * (4 - len(v))))
+        tmp.append(len(v))
         mul.append(tmp)
-    print(sorted(mul, reverse=True))
-            
+
+    mul.sort(reverse=True)
+    for i in mul:
+        result.append(i[0][:i[1]])
+    
+    answer = ''.join(result)
+    
     return answer
 
 print(solution([3, 30, 34, 5, 9]))
+# 반례
+print(solution([20, 201, 41])) # 200000 201111 411111
+# 4120120 -> 4120201
+print(solution([60, 605, 81, 604, 59])) # 202222 210222
+# 454444 449444
+print(solution([0, 0, 0, 0]))
