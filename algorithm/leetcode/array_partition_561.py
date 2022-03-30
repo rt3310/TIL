7700ea5b0e5b859737633caba5f4e72dca220505
rt3310 <btac3310@gmail.com>
2022-03-31 01:27:10 +0900

    update leetcode

--- algorithm/leetcode/array_partition_561.py
+++ algorithm/leetcode/array_partition_561.py
@@ -11,3 +11,22 @@ class Solution:
         return answer
             
 print(Solution().arrayPairSum([6, 2, 6, 5, 1, 2]))
+
+# [풀이1. 오름차순 풀이]==========================================================
+
+class Solution1:
+    def arrayPairSum(self, nums: List[int]):# -> int:
+        sum = 0
+        pair = []
+        nums.sort()
+        
+        for n in nums:
+            # 앞에서부터 오름차순으로 페어를 만들어서 합 계산
+            pair.append(n)
+            if len(pair) == 2:
+                sum += min(pair)
+                pair = []
+        
+        return sum
+            
+print(Solution1().arrayPairSum([6, 2, 6, 5, 1, 2]))
