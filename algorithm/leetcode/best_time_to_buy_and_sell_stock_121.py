149b1d40cdc093b31d1a3a02c2a024343ba6f3c9
rt3310 <btac3310@gmail.com>
2022-04-01 02:30:47 +0900

    update leetcode

--- algorithm/leetcode/best_time_to_buy_and_sell_stock_121.py
+++ algorithm/leetcode/best_time_to_buy_and_sell_stock_121.py
@@ -1,13 +1,19 @@
 from typing import *
 
+from sympy import minpoly
+
 class Solution:
     def maxProfit(self, prices: List[int]): # -> int:
-        answer = 0
-        for i in range(len(prices) - 1):
-            benefit = max(prices[i+1:]) - prices[i]
-            if benefit > answer:
-                answer = benefit
+        min_price = max(prices)
+        max_benefit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            else:
+                benefit = prices[i] - min_price
+                if benefit > max_benefit:
+                    max_benefit = benefit
         
-        return answer
+        return max_benefit
 
 print(Solution().maxProfit([7, 1, 5, 3, 6, 4]))
