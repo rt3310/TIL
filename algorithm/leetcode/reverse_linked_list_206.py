77a2e5929b2ded1c6266f7c7bc4c67d63cfc1fff
rt3310 <btac3310@gmail.com>
2022-04-12 23:29:01 +0900

    update leetcode

--- algorithm/leetcode/reverse_linked_list_206.py
+++ algorithm/leetcode/reverse_linked_list_206.py
@@ -1,3 +1,4 @@
+from operator import ne
 from typing import *
 
 class ListNode:
@@ -15,9 +16,6 @@ class Solution:
             node.next = new_head
             new_head = node
         
-        while new_head:
-            print(new_head.val)
-        
         return new_head
 
 
@@ -34,3 +32,25 @@ a.next.next.next.next = ListNode()
 a.next.next.next.next.val = 5
 
 Solution().reverseList(a)
+
+# [풀이1. 재귀 구조로 뒤집기]==========================================================
+
+class Solution1:
+    def reverseList(self, head: Optional[ListNode]):  # -> Optional[ListNode]:
+        def reverse(node: ListNode, prev: ListNode = None):
+            if not node:
+                return prev
+            next, node.next = node.next, prev
+            return reverse(next, node)
+        
+        return reverse(head)
+
+# [풀이2. 반복 구조로 뒤집기]==========================================================
+
+class Solution2:
+    def reverseList(self, head: Optional[ListNode]):  # -> Optional[ListNode]:
+        node, prev = head, None
+        
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node, next
