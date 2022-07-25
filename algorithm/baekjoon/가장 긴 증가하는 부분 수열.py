581aaba2b5f86affb13116b0b9475ad54020f89b
rt3310 <btac3310@gmail.com>
2022-07-25 22:36:27 +0900

    update beakjoon

--- algorithm/baekjoon/가장 긴 증가하는 부분 수열.py
+++ algorithm/baekjoon/가장 긴 증가하는 부분 수열.py
@@ -2,9 +2,24 @@ n = int(input())
 
 seq = list(map(int, input().split()))
 
-dp = []
+dp = [[0] * n, [0] * n]
 
-dp.append([seq[0]])
+dp[0][0] = seq[0]
+dp[1][0] = 1
 
+for i in range(1, n):
+    if seq[i] > seq[i-1]:
+        dp[0][i] = dp[0][i-1] + 1
+        dp[1][i] = dp[1][i-1]
+        
+    else:
+        if seq[i] >= dp[1][i-1]:
+            dp[0][i] = dp[0][i-1]
+            dp[1][i] = dp[1][i-1]
+            dp[2][i] = dp[2][i-1]
+        else:
+            dp[0][i] = 1
+            dp[1][i] = seq[i]
+            dp[2][i] = dp[2][i-1]            
 
 print(dp)
